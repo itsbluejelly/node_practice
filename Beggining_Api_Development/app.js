c382223f2657d2f2d375bcdc11161c0d05b7999e
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-05 14:00:57 +0300

    start afresh

--- Beggining_Api_Development/app.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const postRoutes = require('./routes/postRoutes')
-const app = express()
-const port = 5500
-
-app.get('/', postRoutes.firstPost)
-app.listen(port, console.log("I made it"))
--- Beggining_Api_Development/routes/postRoutes.js
+++
@@ -1,5 +0,0 @@
-const firstPost = (request, response) => response.send("Hello World Node Js")
-
-module.exports = {
-    firstPost
-}
